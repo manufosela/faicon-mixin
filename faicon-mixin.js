ce3c980b9dc9381c0a338bfe4bd5cee0e3ccffb1
Manuel Juan Fosela Águila <manufosela@gmail.com>
2019-08-30 22:58:52 +0200

    add html litelement import

--- faicon-mixin.js
+++ faicon-mixin.js
@@ -1,9 +1,17 @@
+import { html } from 'lit-element';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 let FavIcon = Base => class extends Base {
-  getFaIcon(iconName) {
+  getNodeFaIcon(iconName) {
     let obj = {prefix: 'fa', iconName: iconName};
     let icn = window.FontAwesome.icon(obj) || {node: ''};
-    return `${icn.node}`;
+    return icn.node;
+  }
+
+  getSVGFaIcon(iconName) {
+    let node = this.getNodeFaIcon(iconName);
+    let svg = node[0];
+    console.log(node[0]);
+    return html`${svg}`;
   }
 };
